3b5654a1db94502e383ad8d7d3445292e4816d7a
Jeniatyt <uvarovevgen1991@gmail.com>
2025-12-11 21:28:43 +0300

    Link to tg chanel

--- src/main/kotlin/com/jeniatyt/button/impl/Button.kt
+++ src/main/kotlin/com/jeniatyt/button/impl/Button.kt
@@ -13,18 +13,21 @@ object Button : AbstractButton() {
     private const val PRICE_STORE_DESCRIPTION = "Прайс «Складские услуги»"
     private const val PRISE_LOGISTICS_DESCRIPTION = "Прайс «Логистика»"
     private const val CALL_MANAGER_DESCRIPTION = "Позвать менеджера"
+    private const val TELEGRAM_CHANEL = "Наш телеграм-канал"
     private const val BACK = "Назад"
     private const val SENT = "Отправить"
     private const val NEXT = "Оставить поле пустым"
 
     private const val SUPPORT_CHAT = "mosfulfillment"
+    private const val CHANEL = "mosfulfill"
 
     val ALL_BUTTON: InlineKeyboardMarkup = creatButtonsCommand(
         listOf(
             CommandButtonSupplier(INDIVIDUAL_CALCULATION_DESCRIPTION, MainCommand.INDIVIDUAL_CALCULATION.getCommand()),
             CommandButtonSupplier(PRICE_STORE_DESCRIPTION, MainCommand.PRAISE_STORE.getCommand()),
             CommandButtonSupplier(PRISE_LOGISTICS_DESCRIPTION, MainCommand.PRAISE_LOGISTICS.getCommand()),
-            RefButtonSupplier(CALL_MANAGER_DESCRIPTION, SUPPORT_CHAT)
+            RefButtonSupplier(CALL_MANAGER_DESCRIPTION, SUPPORT_CHAT),
+            RefButtonSupplier(TELEGRAM_CHANEL, CHANEL),
         )
     )
 
